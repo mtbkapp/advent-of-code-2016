41192e4cd3047a269c9d02839f4526aabb129618
Jason Kapp <jkapp@rallydev.com>
2017-07-10 15:22:25 -0600

    Solve day 2 and refactor

--- src/advent_of_code_2016/day2.clj
+++ src/advent_of_code_2016/day2.clj
@@ -4,84 +4,75 @@
             [clojure.java.io :as io]
             [clojure.test :refer :all]))
 
-(defn up
-  [k]
-  (when-not (#{1 2 3} k)
-    (- k 3)))
 
-(deftest test-up
-  (is (nil? (up 1)))
-  (is (nil? (up 2)))
-  (is (nil? (up 3)))
-  (is (= 1 (up 4)))
-  (is (= 6 (up 9))))
+(defn read-input
+  []
+  (->> (slurp (io/resource "day2_part1.txt"))
+       (string/split-lines)
+       (map seq)))
 
+(defn coords->key
+  [keypad [kx ky]]
+  (get-in keypad [ky kx]))
 
-(defn left
-  [k]
-  (when-not (#{1 4 7} k)
-    (dec k)))
+(defmacro defmove
+  [move-name key-names next-key]
+  `(defn ~move-name
+     [keypad# ~key-names]
+     (let [nk# ~next-key]
+       (when (coords->key keypad# nk#)
+         ~next-key))))
 
-(deftest test-left
-  (is (nil? (left 1)))
-  (is (nil? (left 4)))
-  (is (nil? (left 7)))
-  (is (= 1 (left 2)))
-  (is (= 7 (left 8))))
+(defmove left
+  [kx ky]
+  [(dec kx) ky])
 
+(defmove right
+  [kx ky]
+  [(inc kx) ky])
 
-(defn right
-  [k]
-  (when-not (#{3 6 9} k)
-    (inc k)))
+(defmove up
+  [kx ky]
+  [kx (dec ky)])
 
-(deftest test-right
-  (is (nil? (right 3)))
-  (is (nil? (right 6)))
-  (is (nil? (right 9)))
-  (is (= 2 (right 1)))
-  (is (= 8 (right 7))))
-
-
-(defn down
-  [k]
-  (when-not (#{7 8 9} k)
-    (+ k 3)))
-
-(deftest test-down
-  (is (nil? (down 7)))
-  (is (nil? (down 8)))
-  (is (nil? (down 9)))
-  (is (= 9 (down 6)))
-  (is (= 4 (down 1))))
+(defmove down
+  [kx ky]
+  [kx (inc ky)])
 
 (def move-map
-  {\L left
+  {\U up 
+   \L left
    \R right
-   \U up
    \D down})
 
 (defn move
-  [k dir]
-  (or ((move-map dir) k) k))
+  [keypad k-coord dir]
+  (or ((move-map dir) keypad k-coord) k-coord))
 
-(deftest test-move
-  (is (= 4 (move 5 \L)))
-  (is (= 2 (move 5 \U)))
-  (is (= 8 (move 5 \D)))
-  (is (= 6 (move 5 \R)))
-  (is (= 1 (move 1 \L)))
-  (is (= 1 (move 1 \U)))
-  (is (= 9 (move 9 \R)))
-  (is (= 9 (move 9 \D))))
+(defn solve*
+  [keypad start input]
+  (loop [k start code [] [line & lines] input]
+    (if line
+      (let [next-k (reduce (partial move keypad) k line)]
+        (recur next-k (conj code next-k) lines))
+      code)))
+
+(defn solve
+  [keypad start input]
+  (map (partial coords->key keypad)
+       (solve* keypad start input)))
+
+
+;; Part 1
+
+(def part1-keypad
+  [[1 2 3]
+   [4 5 6]
+   [7 8 9]])
 
 (defn solve-part-1
   [input]
-  (loop [k 5 code [] [line & lines] input]
-    (if line
-      (let [nk (reduce move k line)]
-        (recur nk (conj code nk) lines))
-      code)))
+  (solve part1-keypad [1 1] input))
 
 (def test-input
   [[\U \L \L]
@@ -92,10 +83,23 @@
 (deftest test-solve-part-1
   (is (= [1 9 8 5] (solve-part-1 test-input))))
 
-(defn read-part-1-input
-  []
-  (->> (slurp (io/resource "day2_part1.txt"))
-       (string/split-lines)
-       (map seq)))
+#_(solve-part-1 (read-input))
 
+;; Part 2
+
+(def part2-keypad
+  [[nil nil 1 nil nil]
+   [nil  2  3  4  nil]
+   [ 5   6  7  8   9]
+   [nil \A \B \C  nil]
+   [nil nil \D nil nil]])
+
+(defn solve-part2
+  [input]
+  (solve part2-keypad [0 2] input))
+
+(deftest test-solve-part2
+  (is (= [5 \D \B 3] (solve-part2 test-input))))
+
+#_(solve-part2 (read-input))
 
